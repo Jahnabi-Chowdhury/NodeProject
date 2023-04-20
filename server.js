a5e1fc8581724fdc21c33bce5cdea187f725da5a
Jahnabi Chowdhury <jahnabichowdhury0@gmail.com>
2023-04-20 10:51:23 +0530

    initial project

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res) => {
-    res.send('Hello Node Api')
+app.get('/', (req, res)=> {
+    res.send("Hello Node API")
 })
 
-app.listen(3000, ()=>{
-    console.log('Node Api is running on port 3000')
+app.listen(3000, ()=> {
+    console.log("Node API is running on port 3000");
 })
